134f5a2fafd90e34d6a2b839989fc1e3902abc9b
MathisGD <delaunay.mathis@hotmail.fr>
2021-10-05 00:59:35 +0200

    first front-end to display rooms

--- src/Controller/BedAndBreakfastController.php
+++ src/Controller/BedAndBreakfastController.php
@@ -5,16 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Room;
 
 class BedAndBreakfastController extends AbstractController
 {
     /**
-     * @Route("/bed/and/breakfast", name="bed_and_breakfast")
+     * @Route("/home", name="home")
      */
     public function index(): Response
     {
-        return $this->render('bed_and_breakfast/index.html.twig', [
-            'controller_name' => 'BedAndBreakfastController',
-        ]);
+        $em = $this->getDoctrine()->getManager();
+        $rooms = $em->getRepository(Room::class)->findAll();
+
+        dump($rooms);
+
+        return $this->render(
+            'bed_and_breakfast/index.html.twig',
+            [
+                'rooms' => $rooms
+            ]
+        );
     }
 }
