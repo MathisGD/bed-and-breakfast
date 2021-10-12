71c0ed59db8ebc6a92dd85e810130d4cdd48b22d
MathisGD <delaunay.mathis@hotmail.fr>
2021-10-12 11:01:56 +0200

    Images: add propreties in Room entity

--- src/Form/RoomType.php
+++ src/Form/RoomType.php
@@ -6,6 +6,8 @@ use App\Entity\Room;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class RoomType extends AbstractType
 {
@@ -19,7 +21,9 @@ class RoomType extends AbstractType
             ->add('price')
             ->add('address')
             ->add('owner')
-            ->add('regions');
+            ->add('regions')
+            ->add('imageName', TextType::class,  ['disabled' => true])
+            ->add('imageFile', VichImageType::class, ['required' => false]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
